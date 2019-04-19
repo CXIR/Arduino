7192c0d6cb23136141e9c9a99c402d464c580fe7
CXIR <cxir.developer@gmail.com>
2019-04-19 14:41:26 +0200

    Commit 2, April 19
    Current Date Management

--- wifi_cayenne/wifi_cayenne.ino
+++ wifi_cayenne/wifi_cayenne.ino
@@ -39,17 +39,17 @@ const char* MQTT_PASSWORD = "3ae900a6b871295011df29232f488e7ba4be2ae0";
 const char* CLIENT_ID     = "de76f850-611f-11e9-b189-ab9cb6660d7e";
 
 // THRESHOLDS DEFINITION
-int MIN_TEMPERATURE     = 0;
-int MAX_TEMPERATURE     = 0;
+int MIN_TEMPERATURE     = 10;
+int MAX_TEMPERATURE     = 25;
 int MIN_SOIL_MOISTURE   = 0;
 int MAX_SOIL_MOISTURE   = 0;
-int MIN_SUMMER_HUMIDITY = 0;
-int MIN_WINTER_HUMIDITY = 0;
-int MAX_SUMMER_HUMIDITY = 0;
-int MAX_WINTER_HUMIDITY = 0;
+int MIN_SUMMER_HUMIDITY = 20;
+int MIN_WINTER_HUMIDITY = 20;
+int MAX_SUMMER_HUMIDITY = 60;
+int MAX_WINTER_HUMIDITY = 80;
 
-int START_DAY = 0;
-int END_DAY   = 0;
+int START_DAY = 08;
+int END_DAY   = 20;
 
 // STEP VALUES
 float currentTemperature  = 0.0;
@@ -68,21 +68,30 @@ DHT dht(DHT_SENSOR, DHTTYPE);
 WiFiUDP   ntpUDP;
 NTPClient timeClient(ntpUDP);
 
+String formattedDate;
+String dayStamp;
+String timeStamp;
+
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   
-  pinMode(FAN, OUTPUT);
+  pinMode(FAN,   OUTPUT);
   pinMode(LIGHT, OUTPUT);
-  pinMode(PUMP, OUTPUT);
+  pinMode(PUMP,  OUTPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   
   dht.begin();
   timeClient.begin();
+  timeClient.setTimeOffset(7200);
   
-  Cayenne.begin(MQTT_USERNAME, MQTT_PASSWORD, CLIENT_ID, STASSID, STAPSK);
+  Cayenne.begin(MQTT_USERNAME, 
+                MQTT_PASSWORD, 
+                CLIENT_ID, 
+                STASSID, 
+                STAPSK);
 }
 
 void loop() {
@@ -95,6 +104,13 @@ void loop() {
   fanControl();
   lightControl();
   pumpControl();
+
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+
+  formattedDate = timeClient.getFormattedDate();
+  prepareDate();
 
   Cayenne.loop();
 }
@@ -120,13 +136,14 @@ void fanControl () {
 
 void pumpControl () {
 
-  if(currentSoilMoisture < MIN_SOIL_MOISTURE) {
-    pumpON();
+  if (currentSoilMoisture < MIN_SOIL_MOISTURE) {
+    if (analogRead(WATER_WINGS)) {
+      pumpON();
+    }
   }
-  else if(currentSoilMoisutre > MAX_SOIL_MOISTURE) {
+  else if (currentSoilMoisture > MAX_SOIL_MOISTURE) {
     pumpOFF();
   }
-  // by hour?
 }
 
 void lightControl () {
@@ -140,20 +157,38 @@ void lightControl () {
   else if (digitalRead(LIGHT)) lightOFF();
 }
 
-int getHour () {
+void prepareDate () {
 
+  int splitT = formattedDate.indexOf("T");
+  dayStamp   = formattedDate.substring(0, splitT);
+
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+}
+
+int getDay () {
+  return dayStamp.substring(8, 9);
 }
 
 int getMonth () {
+  return dayStamp.substring(5, 6);
+}
 
+int getYear () {
+  return dayStamp.substring(0, 3);
+}
+
+int getHour () {
+  return timeStamp.substring(0, 1);
 }
 
 int getMinute () {
-
+  return timeStamp.substring(3, 4);
 }
 
 bool checkSeason () {
-
+  if(6 < getMonth() < 9) {
+    isSummer = true;
+  }
 }
 
 void pumpON () {
@@ -186,10 +221,10 @@ void fanOFF () {
 // You can also use functions for specific channels, e.g CAYENNE_OUT(1) for sending channel 1 data.
 CAYENNE_OUT_DEFAULT()
 {
-  Cayenne.virtualWrite(TEMPERATURE_CHANNEL, currentTemperature, "temp", "c" );
-  Cayenne.virtualWrite(HUMIDITY_CHANNEL,   currentHumidity,     "rel_hum", "p");
-  //Cayenne.virtualWrite(WATER_WINGS_CHANNEL,   currentWaterWings,     "rel_hum", "p");
-  Cayenne.virtualWrite(SOIL_MOISTURE_CHANNEL,   currentSoilMoisture,     "soil_moist", "p");
+  Cayenne.virtualWrite(TEMPERATURE_CHANNEL,   currentTemperature,  "temp",       "c");
+  Cayenne.virtualWrite(HUMIDITY_CHANNEL,      currentHumidity,     "rel_hum",    "p");
+  Cayenne.virtualWrite(WATER_WINGS_CHANNEL,   currentWaterWings,   "prox",       "d");
+  Cayenne.virtualWrite(SOIL_MOISTURE_CHANNEL, currentSoilMoisture, "soil_moist", "p");
 }
  
 // Default function for processing actuator commands from the Cayenne Dashboard.
